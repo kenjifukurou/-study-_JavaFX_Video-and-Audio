c02f2b4799ef9a03ae3605f0d57da07bf6fe269d
Kenji Fukurou <59118789+kenjifukurou@users.noreply.github.com>
2021-03-24 00:41:11 +0800

    Add files via upload

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,6 +1,8 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -17,9 +19,15 @@ public class Controller {
     private MediaView mv;
 
     MediaPlayer mediaPlayer;
+    AudioClip clickSFX;
+    AudioClip overlaySFX;
 
 
     public void initialize() {
+
+        // get SoundFX
+        clickSFX = new AudioClip(getClass().getResource("SFXMouseClick.mp3").toString());
+        overlaySFX = new AudioClip(getClass().getResource("SFXMouseOver.mp3").toString());
 
         // get Media a path
         String mediaPath = getClass().getResource("SplittekIntro.mp4").toString();
@@ -28,16 +36,19 @@ public class Controller {
         // setup Media and MediaPlayer
         Media media = new Media(mediaPath);
         mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
 
         // setup MediaView
         mv.setMediaPlayer(mediaPlayer);
         mv.setFitWidth(640);
         mv.setFitHeight(360);
+
     }
 
     @FXML
     void handleButtonPlay(ActionEvent event) {
 //        System.out.println("play button clicked");
+        clickSFX.play();
         if (mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
             mediaPlayer.pause();
         } else {
@@ -48,7 +59,14 @@ public class Controller {
     @FXML
     void handleButtonStop(ActionEvent event) {
 //        System.out.println("stop button clicked");
+        clickSFX.play();
         mediaPlayer.stop();
+    }
+
+    @FXML
+    void handleMouseOver(MouseEvent event) {
+        System.out.println("mouse entered");
+        overlaySFX.play();
     }
 
 }
